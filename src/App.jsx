f1c6cf7f3f5f2ab4d4440a71c43aa9681d0a7fea
cgrade <gradeisageek@gmail.com>
2024-02-12 23:29:07 +0100

    project pushed to github

---
+++ src/App.jsx
@@ -0,0 +1,147 @@
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
+import { Alchemy, Network, Utils } from 'alchemy-sdk';
+import { useState } from 'react';
+import dotenv from 'dotenv';
+
+
+
+
+function App() {
+  const [userAddress, setUserAddress] = useState('');
+  const [results, setResults] = useState([]);
+  const [hasQueried, setHasQueried] = useState(false);
+  const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [isConnected, togleConnect] = useState(false);
+  
+  async function getTokenBalance() {
+    const config = {
+      apiKey: process.env.API_KEY,
+      network: Network.ETH_MAINNET,
+    };
+
+    const alchemy = new Alchemy(config);
+    const data = await alchemy.core.getTokenBalances(userAddress);
+
+    setResults(data);
+
+    const tokenDataPromises = [];
+
+    for (let i = 0; i < data.tokenBalances.length; i++) {
+      const tokenData = alchemy.core.getTokenMetadata(
+        data.tokenBalances[i].contractAddress
+      );
+      tokenDataPromises.push(tokenData);
+    }
+
+    setTokenDataObjects(await Promise.all(tokenDataPromises));
+    setHasQueried(true);
+  }
+
+  async function connectWallet(){
+    // check if metamask is installed
+    if (window.ethereum){
+      if (isConnected){
+        await window.ethereum.request({
+          "method": "wallet_revokePermissions",
+          "params": [
+            {
+              "eth_accounts": {}
+            }
+          ]
+        });
+        togleConnect(!isConnected);
+      } else {
+      
+        // window.ethereum.isConnected ? console.log("is connected") : console.log("not connected1");
+        const addresses = await window.ethereum.request({
+          "method": "eth_requestAccounts",
+          "params": []
+        })
+        setUserAddress(addresses[0]);
+        togleConnect(!isConnected);
+      }
+    }
+  }
+  return (
+    <Box w="100vw">
+      <Center>
+        <Flex
+          alignItems={'center'}
+          justifyContent="center"
+          flexDirection={'column'}
+        >
+          <Heading mb={0} fontSize={36}>
+            ERC-20 Token Indexer
+          </Heading>
+          <Text>
+            Connect your wallet and this website will return all of its ERC-20
+            token balances!
+          </Text>
+        </Flex>
+      </Center>
+      <Flex
+        w="100%"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent={'center'}
+      >
+        <Heading mt={42}>
+          Get all the ERC-20 token balances of this address:
+        </Heading>
+        <Text>
+          {isConnected? userAddress :"CONNECT YOUR WALLET"}
+        </Text>
+        <Button fontSize={20} onClick={connectWallet} mt={36} bgColor="red">
+        {isConnected ? "Disconnect Wallet" : "Connect Wallet"}
+      </Button>
+        <Button fontSize={20} onClick={getTokenBalance} mt={36} bgColor="blue">
+          Check ERC-20 Token Balances
+        </Button>
+
+        <Heading my={36}>ERC-20 token balances:</Heading>
+
+        {hasQueried ? (
+          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
+            {results.tokenBalances.map((e, i) => {
+              return (
+                <Flex
+                  flexDir={'column'}
+                  color="white"
+                  bg="blue"
+                  w={'20vw'}
+                  key={i}
+                >
+                  <Box>
+                    <b>Symbol:</b> ${tokenDataObjects[i].symbol}&nbsp;
+                  </Box>
+                  <Box>
+                    <b>Balance:</b>&nbsp;
+                    {Utils.formatUnits(
+                      e.tokenBalance,
+                      tokenDataObjects[i].decimals
+                    )}
+                  </Box>
+                  <Image src={tokenDataObjects[i].logo} />
+                </Flex>
+              );
+            })}
+          </SimpleGrid>
+        ) : (
+          'Please make a query! This may take a few seconds...'
+        )}
+      </Flex>
+    </Box>
+  );
+}
+
+export default App;
